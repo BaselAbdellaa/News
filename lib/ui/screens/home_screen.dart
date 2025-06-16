78c76bdd4bf87c072a42c6771b1ddb89dc82a92f
Basel Abdellaa <basilahmedabdellah@gmail.com>
2025-06-16 04:16:46 +0300

    home screen code refactored and future builder added

--- lib/ui/screens/home_screen.dart
+++ lib/ui/screens/home_screen.dart
@@ -1,11 +1,12 @@
 import 'dart:math';
 
 import 'package:flutter/material.dart';
+import 'package:news/api/api_manager.dart';
 import 'package:news/core/app_colors.dart';
-import 'package:news/data/dummy_data.dart';
 import 'package:news/data/models/category_model.dart';
-import 'package:news/data/models/news_model.dart';
+import 'package:news/ui/screens/news_sources_view.dart';
 import 'package:news/ui/widgets/custom_app_bar.dart';
+import 'package:news/ui/widgets/loading_widget.dart';
 import 'package:news/ui/widgets/news_list_view.dart';
 
 class HomeScreen extends StatefulWidget {
@@ -18,77 +19,31 @@ class HomeScreen extends StatefulWidget {
 
 class _HomeScreenState extends State<HomeScreen>
     with SingleTickerProviderStateMixin {
-  late CategoryModel categories;
-
-  late TabController _tabController;
-
-  @override
-  void initState() {
-    super.initState();
-    _tabController = TabController(vsync: this, length: dummySources.length);
-    if (!_tabController.indexIsChanging) {
-      fetchNewDataForTab();
-      setState(() {});
-    }
-    fetchNewDataForTab();
-    setState(() {});
-  }
-
-  List<NewsArticle> fetchNewDataForTab() {
-    var randomdata = Random().nextInt(dummyNews.length) + 1;
-    return dummyNews.take(randomdata).toList();
-  }
-
-  @override
-  void dispose() {
-    _tabController.dispose();
-    super.dispose();
-  }
-
-  @override
-  void didChangeDependencies() {
-    categories = ModalRoute.of(context)!.settings.arguments as CategoryModel;
-    super.didChangeDependencies();
-  }
+  late CategoryModel _categories;
 
   @override
   Widget build(BuildContext context) {
-    return DefaultTabController(
-      length: dummySources.length,
-      child: Scaffold(
-        drawer: Drawer(),
-        appBar: CustomAppBar(
-          height: 100,
-          title: categories.name,
-          tabBar: TabBar(
-            controller: _tabController,
-            isScrollable: true,
-            tabs: buildTabs(),
-          ),
-        ),
-        body: TabBarView(
-          controller: _tabController,
-          children: dummySources
-              .map((e) => NewsListView(news: fetchNewDataForTab()))
-              .toList(),
-        ),
+    return Scaffold(
+      drawer: Drawer(),
+      appBar: CustomAppBar(title: _categories.name),
+      body: FutureBuilder(
+        future: ApiManager.getInstance().getSources(_categories.id),
+        builder: (context, snapshot) {
+          if(snapshot.connectionState == ConnectionState.waiting){
+            return LoadingWidget();
+          }else if(snapshot.hasError){
+            return Center(child: Text(snapshot.error.toString()),);
+          }
+          var sources = snapshot.data?.sources;
+          return NewsSourcesView(sources:sources ,);
+        },
       ),
     );
   }
 
-  List<Tab> buildTabs() {
-    return dummySources
-        .map(
-          (source) => Tab(
-            child: Text(
-              source.name ?? "",
-              style: TextStyle(fontSize: 20, color: AppColors.blackColor),
-            ),
-          ),
-        )
-        .toList();
+  @override
+  void didChangeDependencies() {
+    _categories = ModalRoute.of(context)!.settings.arguments as CategoryModel;
+    super.didChangeDependencies();
   }
 }
-
-//test
-//test
