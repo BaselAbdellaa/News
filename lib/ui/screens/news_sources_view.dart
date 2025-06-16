cc6d4e0797d11190cd0843676af9458f01bde654
Basel Abdellaa <basilahmedabdellah@gmail.com>
2025-06-16 23:27:23 +0300

    added initState and dispose for the scroll controller  _ implemented pagination logic _ RefreshIndicator Widget added with its logic

--- lib/ui/screens/news_sources_view.dart
+++ lib/ui/screens/news_sources_view.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:news/api/api_manager.dart';
 import 'package:news/core/app_colors.dart';
+import 'package:news/data/models/NewsResponse/Articles.dart';
 import 'package:news/data/models/sourceReponse/Sources.dart';
 import 'package:news/ui/widgets/loading_widget.dart';
 import 'package:news/ui/widgets/news_list_view.dart';
@@ -44,7 +45,34 @@ class _NewsSourcesViewState extends State<NewsSourcesView>
     // categories = ModalRoute.of(context)!.settings.arguments as CategoryModel;
     super.didChangeDependencies();
   }
+
   var selectedIndex = 0;
+  int page = 1;
+  final _controller = ScrollController();
+  List<NewsArticles> newsList = [];
+  bool nextPage = false;
+  @override
+  void initState() {
+    super.initState();
+
+    // Setup the listener.
+    _controller.addListener(() {
+      if (_controller.position.atEdge) {
+        bool isTop = _controller.position.pixels == 0;
+        if (!isTop) {
+          setState(() {
+            nextPage = true;
+            page++;
+          });
+        }
+      }
+    });
+  }
+@override
+void dispose() {
+    super.dispose();
+    _controller.dispose();
+  }
   @override
   Widget build(BuildContext context) {
     return DefaultTabController(
@@ -58,7 +86,9 @@ class _NewsSourcesViewState extends State<NewsSourcesView>
             onTap: (value) {
               setState(() {
                 selectedIndex = value;
-
+                page = 1;
+                newsList.clear();
+                nextPage = false;
               });
             },
           ),
@@ -68,19 +98,47 @@ class _NewsSourcesViewState extends State<NewsSourcesView>
           //       .map((e) => NewsListView(news: fetchNewDataForTab()))
           //       .toList(),
           // ),
-          FutureBuilder(future: ApiManager.getInstance().getNews(widget.sources![selectedIndex].id??"")
-              , builder: (context, snapshot) {
-              if(snapshot.connectionState == ConnectionState.waiting){
+          FutureBuilder(
+            future: ApiManager.getInstance().getNews(
+              widget.sources![selectedIndex].id ?? "",
+              page: page,
+            ),
+            builder: (context, snapshot) {
+              if (snapshot.connectionState == ConnectionState.waiting &&
+                  newsList.isEmpty) {
                 return LoadingWidget();
-              }else if(snapshot.hasError){
-                return Center(child: Text(snapshot.error.toString()),);
+              } else if (snapshot.hasError) {
+                return Center(child: Text(snapshot.error.toString()));
               }
-              var newsList = snapshot.data?.articles ?? [];
-              return Expanded(child: NewsListView(news: newsList));
-              },)
+              if(newsList.isEmpty || nextPage){
+                newsList.addAll(snapshot.data?.articles ?? []);
+                nextPage=false;
+              }
+
+              print(newsList.length);
+              print(page);
+              return  Expanded(
+                child: RefreshIndicator(
+                  onRefresh: () async {
+                    setState(() {
+                      page = 1;
+                      newsList.clear();
+                    });
+
+                    final response = await ApiManager.getInstance()
+                        .getNews(widget.sources![selectedIndex].id ?? "", page: page);
+
+                    setState(() {
+                      newsList.addAll(response.articles ?? []);
+                    });
+                  },
+                  child: NewsListView(news: newsList, controller: _controller),
+                ),
+              );
+
+            },
+          ),
         ],
-
-
       ),
     );
   }
